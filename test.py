55e6384d5ee4322ace4be09d83528bbab6995cf1
Elliana May <me@mause.me>
2021-10-11 10:04:04 +0800

    Try to override comment handler

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 import phply.phpparse
 from phply import phplex
 
+
+def t_php_DOC_COMMENT(t):
+    raise Exception(t.value)
+
+phplex.t_php_DOC_COMMENT = t_php_DOC_COMMENT
+
 parser = phply.phpparse.make_parser(debug=True)
-print(parser.parse('''<?php echo "hello, world!"; ?>''', lexer=phplex.lexer.clone()))
+print(parser.parse('''<?php echo "hello, world!"; ?>''', lexer=  phplex.FilteredLexer(  phplex.lexer.lexer.clone(phplex))))
