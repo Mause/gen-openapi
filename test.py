fe877936120c1b509b1f1442bef25e2cc5a78340
Elliana May <me@mause.me>
2021-10-11 10:24:23 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,11 +3,13 @@ from urllib.request import urlopen
 import phply.phpparse
 from phply import phplex
 
+comments = []
 
 def t_php_DOC_COMMENT(t):
     r"/\*\*(.|\n)*?\*/"
-    raise Exception(t.value)
-
+    t.lexer.lineno += t.value.count("\n")
+    comments.append(t.value)
+    return t
 
 phplex.t_php_DOC_COMMENT = t_php_DOC_COMMENT
 
@@ -15,7 +17,6 @@ phplex.t_php_DOC_COMMENT = t_php_DOC_COMMENT
 php_text = urlopen(
     "https://github.com/invoiceninja/invoiceninja/raw/v5-develop/app/Http/Controllers/OpenAPI/InvoiceSchema.php"
 ).read()
-print(php_text)
 
 parser = phply.phpparse.make_parser(debug=True)
 print(
@@ -24,3 +25,4 @@ print(
         lexer=phplex.FilteredLexer(phplex.lexer.lexer.clone(phplex)),
     )
 )
+print(comments)
