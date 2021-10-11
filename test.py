aa26d81d55db4b7c733c85b52adfed7f672bf73d
Elliana May <me@mause.me>
2021-10-11 10:15:23 +0800

    Pull from github

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import phply.phpparse
 from phply import phplex
+from urllib.request import url_open
 
 
 def t_php_DOC_COMMENT(t):
@@ -9,10 +10,14 @@ def t_php_DOC_COMMENT(t):
 
 phplex.t_php_DOC_COMMENT = t_php_DOC_COMMENT
 
+
+php_text = urlopen('https://github.com/invoiceninja/invoiceninja/raw/v5-develop/app/Http/Controllers/OpenAPI/InvoiceSchema.php').read()
+print(php_text)
+
 parser = phply.phpparse.make_parser(debug=True)
 print(
     parser.parse(
-        """<?php echo "hello, world!"; ?>""",
+        php_text,
         lexer=phplex.FilteredLexer(phplex.lexer.lexer.clone(phplex)),
     )
 )
