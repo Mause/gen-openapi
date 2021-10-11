82a4c49be07d7377733e20729a6ba56888845a5d
Elliana May <me@mause.me>
2021-10-11 10:00:22 +0800

    Remove breakpoint

--- test.py
+++ test.py
@@ -2,5 +2,4 @@ import phply.phpparse
 from phply import phplex
 
 parser = phply.phpparse.make_parser(debug=True)
-breakpoint()
 print(parser.parse('''<?php echo "hello, world!"; ?>''', lexer=phplex.lexer.clone()))
