f34a9ad6829f0fb1dde0af5502f0e283d3253746
Elliana May <me@mause.me>
2021-10-12 07:08:03 +0000

    Parse full php file

--- test_main.py
+++ test_main.py
@@ -29,5 +29,14 @@ def test_php(snapshot):
  */
  """
     res = list(parse_txt_into_swagger(php))
-    assert res
+    assert all(res)
+    snapshot.assert_match(res)
+
+
+def test_main(snapshot):
+    with open("invoiceninja/InvoiceSchema.php") as fh:
+        res = list(parse_txt_into_swagger(fh.read()))
+    if not all(res):
+        raise SyntaxError()
+    assert all(res)
     snapshot.assert_match(res)
