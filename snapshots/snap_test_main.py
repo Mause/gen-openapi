a08f875cbb46039ebf380a1737ccdea0d98d1b24
Elliana May <me@mause.me>
2021-10-12 03:00:51 +0000

    More tests

--- snapshots/snap_test_main.py
+++ snapshots/snap_test_main.py
@@ -4,4 +4,17 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots["test_simple 1"] = {"args": [("hello", '"world"')], "type": "Schema"}
+snapshots["test_simple[Schema(Property(hello=true))] 1"] = {
+    "args": [{"args": [("hello", "true")], "type": "Property"}],
+    "type": "Schema",
+}
+
+snapshots['test_simple[Schema(hello="world")] 1'] = {
+    "args": [("hello", '"world"')],
+    "type": "Schema",
+}
+
+snapshots["test_simple[Schema(world=true,Property(hello=true))] 1"] = {
+    "args": [("world", "true"), {"args": [("hello", "true")], "type": "Property"}],
+    "type": "Schema",
+}
--- test_main.py
+++ test_main.py
@@ -1,7 +1,17 @@
+from pytest import mark
+
 from main import transform
 
 
-def test_simple(snapshot):
-    schemata = transform("""Schema(hello="world")""")
+@mark.parametrize(
+    "schema",
+    [
+        """Schema(hello="world")""",
+        """Schema(world=true,Property(hello=true))""",
+        """Schema(Property(hello=true))""",
+    ],
+)
+def test_simple(snapshot, schema):
+    schemata = transform(schema)
     assert schemata
     snapshot.assert_match(schemata)
